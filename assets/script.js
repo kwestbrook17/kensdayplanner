5eb740dac73d3b45ee918bef857e229ad5c5ab81
Kenneth Westbrook <kennethwestbrook17@gmail.com>
2023-07-24 08:21:20 -0700

    changed file paths and added current time

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,8 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -20,4 +22,5 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+   $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 });
